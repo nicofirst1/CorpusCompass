f101cc9449d98683df042de5225976904f155a92
dizzi <nicofirst1@gmail.com>
2022-12-14 09:55:09 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TranscriptionTagger',
+      version='1.0',
+      description='Utilities tool for linguistic annotation',
+      author="Nicolo' Brandizzi",
+      author_email='brandizzi@diag.uniroma1.it',
+      url='https://github.com/nicofirst1/TranscriptionTagger',
+      requires= ['nltk'],
+     )
