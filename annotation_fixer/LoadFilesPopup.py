1fbfc587860b4bf4280cd910ef8f17806c2450ed
dizzi <nicofirst1@gmail.com>
2023-02-10 13:01:55 +0100

    fixed pandas loading

--- annotation_fixer/LoadFilesPopup.py
+++ annotation_fixer/LoadFilesPopup.py
@@ -1,7 +1,20 @@
+import pandas as pd
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QFileDialog, QComboBox, QScrollArea
 
 from annotation_fixer.Memory import Memory
+
+
+def load_csv2pandas(path: str, encoding: str, sep: str) -> pd.DataFrame:
+    """
+    Load a csv file into a pandas dataframe
+    """
+    with open(path, "rb") as f:
+        content = f.read().decode(encoding)
+    # find the separator by counting the number of most common separator
+    content = pd.read_csv(path, encoding=encoding, sep=sep, on_bad_lines='skip')
+
+    return content
 
 
 class LoadFilesPopup(QtWidgets.QWidget):
@@ -12,11 +25,17 @@ class LoadFilesPopup(QtWidgets.QWidget):
         self.corpus_files = None
         self.corpus_text = ""
         self.annotation_info = None
+        self.annotation_info_csv = ""
+        self.missing_annotations = None
+        self.missing_annotations_csv = ""
         self.message = None
         self.encodings = ["UTF-8", "UTF-16"]
         enc_idx = self.mem.lfp.get("encoding") or 0
         self.encoding = self.encodings[enc_idx]
         self.setFixedSize(500, 500)
+
+        self.separator = self.mem.lfp.get("separator") or ";"
+        self.mem.lfp["separator"] = self.separator
 
         self.setWindowTitle("Load Files")
 
@@ -25,7 +44,8 @@ class LoadFilesPopup(QtWidgets.QWidget):
     def create_widgets(self):
         self.message = QtWidgets.QLabel("Please upload corpus and annotation:\n"
                                         "- Corpus not uploaded\n"
-                                        "- Annotation not uploaded")
+                                        "- Annotation not uploaded\n"
+                                        "- Missing annotation not uploaded\n")
         self.message.setStyleSheet("color: white; background-color: black")
 
         self.message_scroll = QScrollArea()
@@ -45,6 +65,9 @@ class LoadFilesPopup(QtWidgets.QWidget):
         self.annotation_info_button = QtWidgets.QPushButton("Load Annotation Info")
         self.annotation_info_button.clicked.connect(self.load_annotation_info)
 
+        self.missing_annotation_info_button = QtWidgets.QPushButton("Load Missing Annotation Info")
+        self.missing_annotation_info_button.clicked.connect(self.load_missing_annotation_info)
+
         self.encoding_label = QtWidgets.QLabel("Encoding:")
         self.encoding_combo_box = QComboBox()
         for encoding in self.encodings:
@@ -62,6 +85,7 @@ class LoadFilesPopup(QtWidgets.QWidget):
         layout.addWidget(self.encoding_combo_box)
         layout.addWidget(self.corpus_button)
         layout.addWidget(self.annotation_info_button)
+        layout.addWidget(self.missing_annotation_info_button)
         layout.addWidget(self.message_scroll)
         layout.addWidget(self.corpus_message_scroll)
         layout.addWidget(self.finish_button)
@@ -73,6 +97,9 @@ class LoadFilesPopup(QtWidgets.QWidget):
         self.mem.lfp["encoding"] = index
 
     def load_corpus(self):
+        """
+        Load corpus files
+        """
         dir = self.mem.lfp.get("corpus_dir") or ""
         self.corpus_files = QFileDialog.getOpenFileNames(
             self, "Select Corpus Files", dir, "Text Files (*.txt);;All Files (*.*)"
@@ -96,37 +123,79 @@ class LoadFilesPopup(QtWidgets.QWidget):
             self.message.setText("Please upload corpus and annotation:\n"
                                  "- Corpus file loaded:\n\t--" +
                                  "\n\t--".join(self.corpus_files) +
-                                 "\n- Annotation not uploaded")
+                                 "\n- Annotation not uploaded\n"
+                                 "- Missing annotation not uploaded\n")
             # change color to green
             self.corpus_message.setStyleSheet("color: green; background-color: black")
             self.corpus_message.setText(
                 "Corpus files loaded:\n" + "\n".join(self.corpus_text.split("\n")[:10] + ["..."]))
 
-            if self.annotation_info:
+            if self.annotation_info and self.missing_annotations:
                 self.finish_button.setEnabled(True)
 
     def load_annotation_info(self):
         dir = self.mem.lfp.get("annotation_dir") or ""
         self.annotation_info = QFileDialog.getOpenFileName(
-            self, "Select Annotation Info CSV", dir, "CSV Files (*.csv);;All Files (*.*)"
+            self, "Select Annotation Info CSV", dir, "CSV Files (*.csv)"
         )[0]
 
         if self.annotation_info:
             self.mem.lfp["annotation_dir"] = self.annotation_info.rsplit("/", 1)[0]
 
+            # read annotation info file
+            try:
+                self.annotation_info_csv = load_csv2pandas(self.annotation_info, self.encoding, self.separator)
+            except Exception as e:
+                err_msg = f"Error loading annotation file ({self.annotation_info}):\n{e}\n Please try again"
+                self.corpus_message.setText(err_msg)
+                # change color to red
+                self.corpus_message.setStyleSheet("color: red; background-color: black")
+                return
+
             self.message.setText("Please upload corpus and annotation:\n"
                                  "- Corpus file loaded:\n\t--" +
                                  "\n\t--".join(self.corpus_files) +
-                                 "\n- Annotation file loaded:" + self.annotation_info)
+                                 "\n- Annotation file loaded:" + self.annotation_info + "\n"
+                                                                                        "- Missing annotation not uploaded")
             # change color to green
             self.corpus_message.setStyleSheet("color: green; background-color: black")
             self.corpus_message.setText(
                 "Corpus files loaded:\n" + "\n".join(self.corpus_text.split("\n")[:10] + ["..."]))
 
-            if self.corpus_files:
+            if self.corpus_files and self.missing_annotations:
                 self.finish_button.setEnabled(True)
 
+    def load_missing_annotation_info(self):
+        dir = self.mem.lfp.get("missing_annotation_dir") or ""
+        self.missing_annotations = QFileDialog.getOpenFileName(
+            self, "Select Missing Annotation Info CSV", dir, "CSV Files (*.csv)"
+        )[0]
+
+        if self.missing_annotations:
+            self.mem.lfp["missing_annotation_dir"] = self.missing_annotations.rsplit("/", 1)[0]
+
+            # read annotation info file
+            try:
+                self.missing_annotations_csv = load_csv2pandas(self.missing_annotations, encoding=self.encoding,
+                                                               sep=self.separator)
+
+            except Exception as e:
+                err_msg = f"Error loading missing annotation file ({self.missing_annotations}):\n{e}\n Please try again"
+                self.corpus_message.setText(err_msg)
+                # change color to red
+                self.corpus_message.setStyleSheet("color: red; background-color: black")
+                return
+
+        self.message.setText("Please upload corpus and annotation:\n"
+                             "- Corpus file loaded:\n\t--" +
+                             "\n\t--".join(self.corpus_files) +
+                             "\n- Annotation file loaded:" + self.annotation_info
+                             + "\n- Missing annotation file loaded:" + self.missing_annotations)
+
+        if self.corpus_files and self.annotation_info:
+            self.finish_button.setEnabled(True)
+
     def finish(self):
-        if self.corpus_text and self.annotation_info:
+        if self.corpus_text and self.annotation_info and self.missing_annotations:
             # close window
             self.close()
