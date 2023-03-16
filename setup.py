52d3fbb99c105b36e67dd1805d614035021e19e1
dizzi <nicofirst1@gmail.com>
2023-03-16 11:47:17 +0100

    setup fixes

--- setup.py
+++ setup.py
@@ -1,24 +1,11 @@
 from setuptools import setup
 
-# common requirements for all packages
-common_requirements = ['pandas', 'numpy']
-
 setup(
-    name='CorpusCompass',
-    version='2.0',
-    description='Utilities tool for linguistic annotation',
+    name="CorpusCompass",
+    version="1.0",
+    description="Utilities tool for linguistic annotation",
     author="Nicolo' Brandizzi",
-    author_email='brandizzi@diag.uniroma1.it',
-    url='https://github.com/nicofirst1/CorpusCompass',
-    install_requires=common_requirements,
-    packages=[
-        'annotation_fixer',
-        'dataset_analyzer',
-        'dataset_creator',
-    ],
-    extras_require={
-        'annotation_fixer': ['PySide6'],
-        'dataset_analyzer': ['nltk', 'matplotlib', 'statsmodels', 'scikit-learn'],
-        'dataset_creator': ['tqdm', 'nltk']
-    }
+    author_email="brandizzi@diag.uniroma1.it",
+    url="https://github.com/nicofirst1/CorpusCompass",
+    install_requires=["tqdm", "nltk", "pandas", "numpy"],
 )
